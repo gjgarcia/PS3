63e7a04ed32fe6ba146962068eafcd80ba2a0847
jongarcia <jongarcia@wifi-roaming-128-4-220-223.host.udel.edu>
2014-09-17 20:08:03 -0400

    PS3 COMMIT

---
+++ src/MainPackage/Account.java
@@ -0,0 +1,67 @@
+package MainPackage;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.naming.InsufficientResourcesException;
+
+public class Account {
+	private int id = 0;
+	private double balance = 0;
+	private double annualInterestRate = 0;
+	private java.util.Date createdDate;
+	Scanner user_input = new Scanner(System.in);
+
+	public Account() {
+	}
+	public Account(int id, double balance, double annualInterestRate){
+		this.setId(id);
+		this.setBalance(balance);
+		this.setAnnualInterestRate(annualInterestRate);
+		this.createdDate= new java.util.Date();
+		}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+	public java.util.Date getCreatedDate() {
+		return createdDate;
+	}
+	public double getMonthlyInterestRate(){
+		return balance*((annualInterestRate/100)/12);
+	}
+
+	public void withdraw(double amount) throws InsufficientFundsException{
+		if (amount<= balance)
+		{
+		balance-=amount;
+	}
+		else
+		{
+			double needs= amount-balance;
+			throw new InsufficientFundsException(needs);
+		}
+		}
+		public void deposit(double amount)
+		{
+			balance += amount;
+		}
+		}
+		
+	
+	
+
